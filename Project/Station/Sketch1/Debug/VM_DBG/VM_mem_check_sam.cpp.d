6ef6af00a44940f186435bff1b5477d0e5c08d23
sergey-zalukaev <sergey.zalukaev@playrix.com>
2021-11-02 18:35:31 +0300

    Новый проект

--- Project/Station/Sketch1/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\zaluk\AppData\Local\Temp\VMBuilds\Sketch1\mega_atmega2560\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\v0m3gso5.bks\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
---
+++ Project/Station/Station.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Station.ino
+    Created:	02.11.2021 18:28:20
+    Author:     DESKTOP-H2OP66D\zaluk
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
