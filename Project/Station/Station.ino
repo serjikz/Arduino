e26164e48c9ece9b88870d138cbce23948f521a0
sergey-zalukaev <sergey.zalukaev@playrix.com>
2021-11-02 18:40:49 +0300

    Компиляция проекта

--- Project/Station/Station.ino
+++ Project/Station/Station.ino
@@ -1,32 +1,13 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       Station.ino
-    Created:	02.11.2021 18:28:20
-    Author:     DESKTOP-H2OP66D\zaluk
-*/
+#include "src/BMP280Sensor.h"
 
-// Define User Types below here or use a .h file
-//
-
-
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
-//
-
-// The setup() function runs once each time the micro-controller starts
-void setup()
-{
-
-
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("BMP280 test"));
+    Sensors::BMP280.Init();
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
-{
-
-
+void loop() {
+    Serial.println(Sensors::BMP280.GetTemperature());
+    Serial.println(Sensors::BMP280.GetPressure());
+    delay(4000);
 }
