050084b66c18e526d5a9d55b6f048834f3efa525
Sergey Zalukaev <sergey.zalukaev@playrix.com>
2024-08-21 21:45:44 +0300

    ENS160AHT2X

--- Projects/Garage/src/main.cpp
+++ Projects/Garage/src/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
-#include "ScioSense_ENS160.h" 
 #include <LCD1602Display.h>
 #include <RGYColorsIniter.h>
+#include <ENS160AHT2X.h>
 #include <printf.h>
 
 const int GLOBAL_DELAY_TIME = 3000;
 unsigned long long time = 0;
 
-RGYColorsIniter rgyIniter(PIND3, PIND2, PIND4);
+RGYColorsIniter rgyIniter(PIND2, PIND3, PIND4);
+ENS160AHT2X ensAht2x(String("ENS160AHT2X"));
 
 void setup()
 {    
     Serial.begin(9600);
     Wire.begin();
     printf_begin();
-    Display::LCD1602.Init();
+    
     rgyIniter.Start();
+    Display::LCD1602.Init();
+    ensAht2x.Init();
+    
 }
 
 void loop()
