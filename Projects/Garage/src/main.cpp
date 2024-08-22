b09840c222394394106e01a00963edc665e7a8cb
Sergey Zalukaev <sergey.zalukaev@playrix.com>
2024-08-22 13:35:50 +0300

    Обработка ошибки подключения

--- Projects/Garage/src/main.cpp
+++ Projects/Garage/src/main.cpp
@@ -7,7 +7,7 @@
 const int GLOBAL_DELAY_TIME = 3000;
 unsigned long long time = 0;
 
-RGYColorsIniter rgyIniter(PIND2, PIND3, PIND4);
+RGYDiodsInformer rgyDiodsInformer(PIND2, PIND3, PIND4);
 ENS160AHT2X ensAht2x(String("ENS160AHT2X"));
 
 void setup()
@@ -16,20 +16,45 @@ void setup()
     Wire.begin();
     printf_begin();
     
-    rgyIniter.Start();
+    rgyDiodsInformer.Start();
     Display::LCD1602.Init();
-    ensAht2x.Init();
-    
+    ensAht2x.Init();   
+}
+
+void DisplaySensorData()
+{
+    Serial.println(ensAht2x.GetTemperature());
+    Serial.println(ensAht2x.GetHumidity());
+    rgyDiodsInformer.BlinkGreenLight();
+
+    String vals[2];
+    vals[0] = ensAht2x.GetTemperature();
+    vals[1] = ensAht2x.GetHumidity();
+    Display::LCD1602.Clear();
+    Display::LCD1602.UpdateValues(vals);
+}
+
+void ShowErrorState()
+{
+    rgyDiodsInformer.BlinkRedLight();
+    if (Display::LCD1602.IsInited()) {
+        Display::LCD1602.Clear();
+        Display::LCD1602.ShowErrMsg();
+    }
 }
 
 void loop()
 {
-    rgyIniter.Update();
+    rgyDiodsInformer.Update();
+    Display::LCD1602.Update();
+    Display::LCD1602.ShowStoragedData();
+    
     if (millis() - time >= GLOBAL_DELAY_TIME) {
-        time = millis();
-        // Display::LCD1602.Clear();  
-        // Display::LCD1602.Print(0, 0, internalT);
-        // Display::LCD1602.Print(0, 1, externalT);   
-        rgyIniter.BlinkGreenLight();
+        time = millis();        
+        if (ensAht2x.IsInited()) {
+           DisplaySensorData();
+        } else {
+            ShowErrorState();
+        }
     }
 }
